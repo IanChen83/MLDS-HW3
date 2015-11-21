4a68ffbc61f01621de9a2744d767bd8b6ae16858
Patrick Chen <patrickchen1994@gmail.com>
2015-11-21 22:25:23 +0800

    debug data.py

--- HMM/data.py
+++ HMM/data.py
@@ -18,11 +18,15 @@ def trimming(data):
         m = type_convert.index(phoneme)
         return m + 97 if m < 26 else m + 39
 
-    # Just wondering will it simply work ? XDDD
-    # ret = ["".join([conversion(i) for i in sentence]) for sentence in data]
     ret = []
     for sentence in data:
-        x = [conversion(i) for i in sentence]
+        last = None
+        x = []
+        for i in sentence:
+            if i == last:
+                continue
+            last = i
+            x.append(conversion(i))
         if x[0] == 'L':
             x.pop(0)
         if x[-1] == 'L':
@@ -31,6 +35,36 @@ def trimming(data):
 
     return ret
 
+def trimming_3(data):
+    """
+        The same as trimming(data) except that only 3-in-a-row phonemes will remain
+    """
+    type_convert = [line.split()[0] for line in open("38_idx_char.map", 'r')]
+    def conversion(phoneme):
+        m = type_convert.index(phoneme)
+        return m + 97 if m < 26 else m + 39
+
+    ret = []
+    for sentence in data:
+        x = []
+        counter = 0
+        last = None
+        for i in sentence:
+            if i == last:
+                counter += 1
+                continue
+            last = i
+            if counter < 3:
+                x.pop(-1)
+            x.append(conversion(i))
+            counter = 1
+        if x[0] == 'L':
+            x.pop(0)
+        if x[-1] == 'L':
+            x.pop(-1)
+        ret.append("".join(x))
+
+    return ret
 
 def convert_48_to_39(data):
     """
