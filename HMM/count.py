41c1a392f18cffe6f2d2b6abb088f4988c0b4506
Patrick Chen <patrickchen1994@gmail.com>
2015-11-22 20:36:37 +0800

    Debugging count.py finished Debugging hmm.py unfinished

--- HMM/count.py
+++ HMM/count.py
@@ -1,7 +1,6 @@
 import numpy
-
-input_y_file = "train_y.txt"
-input_type_file = "48_idx_char.map"
+from data import type_table
+from data import load_answers
 output_trans_file = "count.map"
 ################### Data structure ########################
 # A  50x50 matrix mat in which
@@ -9,43 +8,33 @@ output_trans_file = "count.map"
 # 0 is the START state
 # 49 means the END state
 mat = numpy.zeros((50, 50))
-type_table = ["^"]
+ans = load_answers("answer_map.txt")
 
-input_y = open(input_y_file, 'r')
-input_type = open(input_type_file, 'r')
+for seq in ans:
+    last = 0
+    for i in seq:
+        mat[last][i + 1] += 1
+        last = i + 1
+    mat[last][49] += 1
 
-# Get type_table
-for line in input_type:
-    token = line.split()[0]
-    type_table.append(token)
-
-type_table.append("$")
-
-# Example  maeb0_si1411_1,sil
-
-# Start counting
-last_header = ["", ""]
-last_idx = 0
-for line in input_y:
-    line_split = line.split(",")
-    header = line_split[0].split("_")
-    token = line_split[1].split()[0]
-
-    if last_header[0] != header[0] or last_header[1] != header[1]:
-        mat[last_idx][49] += 1
-        last_header = ["", ""]
-        last_idx = 0
-
-    idx = type_table.index(token)
-    mat[last_idx][idx] += 1
-
-    last_header[0], last_header[1] = header[0], header[1]
-    last_idx = idx
-
-mat[last_idx][49] += 1
-
-output_trans = open(output_trans_file, 'w')
+output = open(output_trans_file, 'w')
+s = '^'
+e = '$'
 for i in range(50):
     for j in range(50):
-        output_trans.write("%s %s %d\n" % (type_table[i], type_table[j], int(mat[i][j])))
-print(mat.sum())
+        if i == 0:
+            s = '^'
+        elif i == 49:
+            s = '$'
+        else:
+            s = type_table[i - 1]
+
+        if j == 0:
+            e = '^'
+        elif j == 49:
+            e = '$'
+        else:
+            e = type_table[j - 1]
+
+        output.write("%s %s %d\n" % (s,e,mat[i][j]))
+
--- HMM/hmm.py
+++ HMM/hmm.py
@@ -4,32 +4,63 @@ from data import type_table
 
 input_transition_file = "count.map"
 input_transition = open(input_transition_file, 'r')
-transition = numpy.zeros((50, 50))
+transition = numpy.zeros((48, 48))
+start_prob = [0] * 48
+end_prob = [0] * 48
 z = [_z.rstrip() for _z in open(input_transition_file, 'r')]
 for line in z:
     _3 = line.split()
-    transition[type_table.index(_3[0])][type_table.index(_3[1])] = int(_3[2])
+    if _3[0] == '^':
+        if _3[1] == '$' or _3[1] == '^':
+            continue
+        else:
+            start_prob[type_table.index(_3[1])] = int(_3[2])
+    elif _3[0] == '$':
+        continue
+    else:
+        if _3[1] == '^':
+            continue
+        elif _3[1] == '$':
+            end_prob[type_table.index(_3[0])] = int(_3[2])
+        else:
+            transition[type_table.index(_3[0])][type_table.index(_3[1])] = int(_3[2])
 
-
-def viterbi(emission, observed):
+def viterbi(emission):
+    """
+        Input   : A (n x 48) sequence
+                    [
+                        [0.111, 0.352, 0.522, ...],
+                        [0.412, 0.241, 0.251, ...],
+                        ...
+                    ]
+        Output  : The (1 x n) path that has biggest probability
+                    [sil, sil, sil, ...]
+    """
     stack = []
-    last_prob = [1] * 50
+    last_prob = start_prob
 
-    for i in range(len(observed)):
-        prob = [0] * 50
+    for i in range(len(emission)):
+        prob = [0] * 48
         maximum = 0
         max_last = 0
         for k in range(48):         # Current observed
             local_max = 0
             for j in range(48):     # Last observed
-                t = last_prob[j + 1] * transition[j + 1][k + 1] * emission[i][k]
+                t = last_prob[j] * transition[j][k] * emission[i][k]
                 if t > local_max:
                     local_max = t
                     if t > maximum:
                         maximum = t
                         max_last = j
-            prob[k + 1] = local_max
+            prob[k] = local_max
         stack.append(type_table[max_last])
-        last_prob = prob
+        last_prob = prob if prob[0] > 10000 else [x/10000 for x in prob]
+    for i in range(len(last_prob)):
+        last_prob[i] = last_prob[i] * end_prob[i]
     stack.append(type_table[last_prob.index(max(last_prob))])
     return stack
+
+a = data.load_sequences("dnn_softmax.ark")
+b = data.get_answer_from_seq(a)
+c = viterbi(a[0])
+print(c)
