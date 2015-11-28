027d463520ee804be065c09cf8dbbd596c956b72
Patrick Chen <patrickchen1994@gmail.com>
2015-11-28 22:49:06 +0800

    Add general purpose viterbi function

--- viterbi.cpp
+++ viterbi.cpp
@@ -1,10 +1,8 @@
-#include <list>
-#include <iostream>
-#include <iomanip>
-#include "data.h"
+#include "viterbi.h"
 using namespace std;
 
 /****************** Viterbi Algorithm *********************
+ * Parameters **
  * Obv  : int
  *      | Number of elements in the observation space
  *
@@ -23,21 +21,10 @@ using namespace std;
  * End  : vector
  *      | End probibility
  *
- * Return *
- * ret  : list<Sta>
+ * Return **
+ * ret  : list<int>
+ *      | int: index in the state space
  *********************************************************/
-template<typename T>
-class value_idx{
-    public:
-        T value;
-        int index;
-        void assign(T& v, int& idx){
-            v = value;
-            idx = index;
-        };
-        value_idx(T v, int i): value(v), index(i){};
-};
-
 template<typename T>
 value_idx<T> inline max(T* v, int total){
     T* x = v;
@@ -63,22 +50,39 @@ void inline print(T* v){
 
 template<int obv, int sta>
 list<int>
-viterbi(const double** Em, const double** Tm, const double* start, const double* end){
+viterbi(double** em, double** tm, double* start, double* end){
     // Initialization
+    double Em[obv][sta];
+    double Tm[sta][sta];
+    for(int i = 0; i < obv; ++i){
+        for(int j = 0; j < sta; ++j){
+            Em[i][j] = log(em[i][j]);
+//            Em[i][j] = em[i][j];
+        }
+    }
+    for(int i = 0; i < sta; ++i){
+        for(int j = 0; j < sta; ++j){
+            Tm[i][j] = log(tm[i][j]);
+//            Tm[i][j] = tm[i][j];
+        }
+    }
+
     double prob[obv][sta];
     int backTrack[obv][sta];
     double temp_prob[sta];
 
     // Head probability
     for(int i = 0; i < sta; ++i){
-        prob[0][i] = start[i] + Em[0][i];
+        prob[0][i] = log(start[i]) + Em[0][i];
+//        prob[0][i] = start[i] * Em[0][i];
         backTrack[0][i] = 0;
     }
 
     for(int i = 1; i < obv; ++i){
         for(int j = 0; j < sta; ++j){
-            for(int k = 0; k < sta; ++j){
+            for(int k = 0; k < sta; ++k){
                 temp_prob[k] = prob[i-1][k] + Em[i][j] + Tm[k][j];
+//                temp_prob[k] = prob[i-1][k] * Em[i][j] * Tm[k][j];
             }
             max(temp_prob, sta).assign(prob[i][j], backTrack[i][j]);
         }
@@ -86,7 +90,8 @@ viterbi(const double** Em, const double** Tm, const double* start, const double*
 
     // End probability
     for(int i = 0; i < sta; ++i){
-        temp_prob[i] = prob[obv-1][i] * end[i];
+        temp_prob[i] = prob[obv-1][i] + log(end[i]);
+//        temp_prob[i] = prob[obv-1][i] * end[i];
     }
     double final = 0;
     int index = 0;
@@ -101,10 +106,34 @@ viterbi(const double** Em, const double** Tm, const double* start, const double*
 
 }
 
-
+/************* Example Usage ********************
+ *
 int main(){
-    Sequences seqs;
-    LoadSequences(seqs, "HMM/dnn_softmax_sub.ark");
 
+    // Initialization
+    double** trans = new double*[2];
+    trans[0] = new double[2];
+    trans[1] = new double[2];
+    trans[0][0] = 0.7;
+    trans[0][1] = 0.3;
+    trans[1][0] = 0.4;
+    trans[1][1] = 0.6;
+    double** emit = new double*[3];
+    emit[0] = new double[2];
+    emit[1] = new double[2];
+    emit[2] = new double[2];
+    emit[0][0] = 0.4;
+    emit[0][1] = 0.1;
+    emit[1][0] = 0.2;
+    emit[1][1] = 0.5;
+    emit[2][0] = 0.2;
+    emit[2][1] = 0.5;
+
+    double start[2] = {0.8, 0.2};
+    double end[2] = {1, 1};
+
+    // Notice the template parameter!
+    auto ret = viterbi<3,2>(emit,trans,start,end);
     return 0;
 }
+************************************************/
---
+++ viterbi.h
@@ -0,0 +1,19 @@
+#include <list>
+#include <iomanip>
+#include "data.h"
+
+template<typename T>
+class value_idx{
+    public:
+        T value;
+        int index;
+        void assign(T& v, int& idx){
+            v = value;
+            idx = index;
+        };
+        value_idx(T v, int i): value(v), index(i){};
+};
+
+template<typename T> value_idx<T> inline max(T*, int);
+template<typename T, int a, int num> void inline print(T*);
+template<int obv, int sta> list<int> viterbi(double**, double**, double*, double*);
